5e610ea6bee1924ec4a9ab7edcdb3cc1ca7d4f4f
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2023-12-11 08:18:22 +0530

    updated code

---
+++ Assignment/Functions Assignment/Question5.js
@@ -0,0 +1,29 @@
+function calculateFactorial (n) {
+    let facValue = n
+    let facArray = [];
+
+    // Check value is number or not:
+    if (n !== 'Number') {
+        let value = parseInt(n);
+        if (value < 0) {
+            console.log("Enter Valid Number");
+            return;
+        } else if ( value >= 0) {
+            facValue = value;
+        }
+    // Check Value is not negative:
+    if ( n < 0) {
+
+    }
+
+    }
+    // Check value is greater than 0:
+    let zeroCheck = function (x) {
+        if (x === 0) {
+            facArray.push(1);
+        }
+    }
+    const facCal = function () {
+
+    }
+}
