1175229a889526fa2b1608ef0e6d1f184eeb2a88
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2023-12-13 20:38:04 +0530

    updated

--- Lecture/Nov_25 HW/Problem3.js
+++ Lecture/Nov_25 HW/Problem3.js
@@ -1,26 +1,44 @@
 // Sanity Check + More Than Zero Function
 const sanityCheck = (value) => {
-    let input = value;
+  let input = value;
 
-    if (typeof value !== "number") {
-        input = parseInt(value);
-        if (isNaN(value)) {
-            console.log("Enter Valid Number");
-            return;
-        }
-    } else if (value <= 0) {
-        console.log(`Enter value bigger than zero`);
-        return
+  if (typeof value !== "number") {
+    input = parseInt(value);
+    if (isNaN(value)) {
+      console.log("Enter Valid Number");
+      return;
     }
-    console.log("Yes", typeof value);
-    return input
-}
+  } else if (value <= 0) {
+    console.log(`Enter value bigger than zero`);
+    return;
+  }
+  return input;
+};
 
-const fizzBuzz = (input) => {
-    let checkedValue = sanityCheck(input);
-    let fizzBuzzArray = []
-    
-    if (typeof checkedValue === "undefined") {
-        return; 
+const factorialValue = (factInput) => {
+  let checkedValue = sanityCheck(factInput);
+
+  if (typeof checkedValue === "undefined") {
+    return;
+  }
+
+  function factorialCalc(param) {
+    if (param === 0) {
+      return 1;
+    } else {
+      return param * factorialCalc(param - 1);
     }
-}
+  }
+  let retunValue = factorialCalc(checkedValue);
+  console.log(`Factorial of ${checkedValue} is ${retunValue}`);
+  return retunValue;
+};
+
+factorialValue(5);
+// Factorial of 5 is 120
+factorialValue("25");
+// Factorial of 25 is 1.5511210043330986e+25
+factorialValue("a");
+// Enter Valid Number
+factorialValue(-1);
+// Enter value bigger than zero
