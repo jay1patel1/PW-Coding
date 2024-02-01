e1070bad9dc8ed53d56555152358fb35d2b4f838
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-02 00:44:58 +0530

    updated new file

--- Project/Project-4/tailwind.config.js
+++ Project/Project-4/tailwind.config.js
@@ -4,20 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'g_LightBlue':'#eaf1fb',
-        'g_LightBlueSelect':'#d3e3fd',
-        'g_BleCompose':'#c2e7ff',
-        'g_BGGray':'#f6f8fc',
-        'g_inbox':'#f2f6fc',
-        'g_Oranges':'#f4b400',
-        'g_fonts':'#616469',
-        'g_white':'#ffffff',
-        'g_grayHover' : '#dadce0',
-        'g_active' : '#eaf1fb',
-        'g_icon_select' : '#041e49',
-        'g_icon' : '#444746',
-        'g_active_hover' : '#e4ecf8',
-        'g_red' : '#b3261e',
+        'google_side':'#eaf1fb',
+        'google_fontColor':'rgb(95, 99, 104)',
+        'google_hoverBG':'#dde3ec',
+        'google_slideOut':'#f6f8fc',
       }
     },
   },
