3e4e0866d9a6ee3e2614840467bcb3dcbb14bf23
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-13 19:06:09 +0530

    updated

--- Project/Project-6/tailwind.config.js
+++ Project/Project-6/tailwind.config.js
@@ -3,10 +3,28 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      font1: ['Kalam', "cursive"],
+      font2: ['Permanent Marker', "cursive"],
+      font3: ['Sixtyfour', "sans-serif"]
+    }
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          accent: "#f44236",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          accent: "#f44236",
+        },
+      },
+    ],
   },
 }
 
