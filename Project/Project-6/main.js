fe72f32165033893afa79a7ff127e4caf6e45681
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2024-02-13 04:08:07 +0000

    start writing logic

---
+++ Project/Project-6/main.js
@@ -0,0 +1,21 @@
+class Question {
+    constructor (question,option1,option2,option3,option4,correctAnswerIndex) {
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.correctAnswer = correctAnswerIndex;
+    }
+}
+
+class QuestionBank {
+    constructor () {
+        this.questionArray = [];
+    }
+
+    qAdd (question,option1,option2,option3,option4,correctAnswerIndex) {
+        const newQuestion = new Question(question,option1,option2,option3,option4,correctAnswerIndex);
+        this.questionArray.push(newQuestion);
+    };
+}
